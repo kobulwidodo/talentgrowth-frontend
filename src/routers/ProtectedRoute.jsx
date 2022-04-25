5bd0bc1c18ad64b01413d1aa4080ecbd2bff1e22
kobulwidodo <fadhilhan01@gmail.com>
2022-04-26 03:19:06 +0700

    feat: init protected route

---
+++ src/routers/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useUserContext } from "../context/userContext";
+import useSnackbar from "../hooks/useSnackbar";
+
+const ProtectedRoute = () => {
+  const { isAuthenticated } = useUserContext();
+  const snackbar = useSnackbar();
+
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
+
+  snackbar.error("Login required");
+  return <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
