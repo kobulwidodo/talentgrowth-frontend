cf4dca491786ec2034d55fd269ce3a56da8fcd0f
kobulwidodo <fadhilhan01@gmail.com>
2022-04-26 03:14:59 +0700

    feat: integrate user context

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,18 +1,21 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import logo from "../../assets/logo/logo.png";
 import logoFull from "../../assets/logo/logo-full.png";
-import { NavLink } from "react-router-dom";
+import ButtonPrimary from "../ButtonPrimary";
+import { Link, NavLink } from "react-router-dom";
 import classNames from "classnames";
+import { useUserContext } from "../../context/userContext";
 
 const Navbar = () => {
   const navigation = [
     { name: "Home", href: "/" },
     { name: "Internship", href: "/internship" },
-    { name: "Webinar", href: "/webinar" },
-    { name: "Bootcamp", href: "/bootcamp" },
+    { name: "Webinar", href: "/event/webinar" },
+    { name: "Bootcamp", href: "/event/course" },
   ];
+
+  const { isAuthenticated, userInfo, logout } = useUserContext();
 
   return (
     <Disclosure as="nav" className="bg-white drop-shadow-sm">
@@ -66,70 +69,31 @@ const Navbar = () => {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
+                {isAuthenticated ? (
+                  <>
+                    <Link to="/me">Hi, {userInfo?.name}</Link>
+                    <div className="hidden sm:flex">
+                      <div className="mx-2">|</div>
+                      <button onClick={logout}>Logout</button>
+                    </div>
+                  </>
+                ) : (
+                  <div className="gap-5 hidden sm:flex">
+                    <ButtonPrimary
+                      className=""
+                      isLink={true}
+                      label="Login"
+                      to="/login"
+                    />
+                    <ButtonPrimary
+                      className=""
+                      isLink={true}
+                      label="Register"
+                      to="/register"
+                      type="secondary"
+                    />
                   </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                )}
               </div>
             </div>
           </div>
@@ -137,21 +101,44 @@ const Navbar = () => {
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <Link
                   key={item.name}
-                  as="a"
-                  href={item.href}
+                  to={item.href}
                   className={classNames(
                     item.current
                       ? "bg-gray-200 text-gray-700"
                       : "text-gray-700 hover:bg-gray-100",
                     "block px-3 py-2 rounded-md text-base font-medium"
                   )}
-                  aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </Link>
               ))}
+              {!isAuthenticated ? (
+                <>
+                  <ButtonPrimary
+                    className="block"
+                    isLink={true}
+                    label="Login"
+                    to="/login"
+                  />
+                  <ButtonPrimary
+                    className="block"
+                    isLink={true}
+                    label="Register"
+                    to="/register"
+                    type="secondary"
+                  />
+                </>
+              ) : (
+                <ButtonPrimary
+                  className="block w-full"
+                  isLink={false}
+                  label="Logout"
+                  onClick={logout}
+                  type="secondary"
+                />
+              )}
             </div>
           </Disclosure.Panel>
         </>
