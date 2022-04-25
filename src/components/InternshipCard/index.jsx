489285eddc321c9f0edc0bda5c99cb71493d0e2e
kobulwidodo <fadhilhan01@gmail.com>
2022-04-26 03:15:16 +0700

    feat: add internship card component

--- src/components/InternshipCard/index.jsx
+++ src/components/InternshipCard/index.jsx
@@ -15,14 +15,14 @@ const InternshipCard = ({
       className="bg-[#FAFAFA] hover:bg-[#f3f3f3] px-5 py-4 drop-shadow-sm rounded-xl"
     >
       <div className="flex flex-row">
-        <img src={srcLogo} className="mr-5" alt="" srcset="" />
+        <img src={srcLogo} className="mr-5" alt="" srcSet="" />
         <div className="">
-          <p className="text-xl font-medium text-[#211B3D]">{position}</p>
-          <p className="text-base font-normal text-[#2E405C]">{company}</p>
-          <p>{isPaid ? "Paid" : "Unpaid"} Internship</p>
+          <p className="text-base font-medium text-[#211B3D]">{position}</p>
+          <p className="text-sm font-normal text-[#2E405C]">{company}</p>
+          <p className="text-sm">{isPaid ? "Paid" : "Unpaid"} Internship</p>
         </div>
       </div>
-      <p className="text-right mt-2">{created}</p>
+      <p className="text-right mt-3">{created}</p>
     </Link>
   );
 };
