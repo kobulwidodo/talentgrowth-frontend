43b8eae0d50cc61ef495b2700d6ede21df6181bb
kobulwidodo <fadhilhan01@gmail.com>
2022-04-26 03:18:57 +0700

    feat: init auth route

---
+++ src/routers/AuthRoute.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useUserContext } from "../context/userContext";
+
+const AuthRoute = () => {
+  const { isAuthenticated } = useUserContext();
+
+  if (isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
+};
+
+export default AuthRoute;
